8da2a9937a4ae08ad7d2fa4f8e0b9b7a4963c8f6
shivveerq <40211699+shivveerq@users.noreply.github.com>
2018-06-28 10:31:14 +0530

    Add files via upload

---
+++ empinfo.py
@@ -0,0 +1,9 @@
+#input value from key board
+Eno=int (input("Enter Emp. Number :"))
+ename= str (input("Enter Emp. Name :"))
+Esal=float (inut("Enter emp. Salary : "))
+marrid=bool (input("Enter Marrid Status of Employee :"))
+print("Emp. Id is :",Eno)
+print("Emp. Name is :",Ename)
+print("Emp. salary is:",Esal)
+print("Emp. Marrid Status is:",marrid)
